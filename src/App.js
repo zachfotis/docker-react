55710b52d5d41f54c1041235b214b482161d832b
Fotios N. Zachopoulos <zaxfotis@gmail.com>
2023-06-26 00:44:51 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
@@ -10,13 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React!
         </a>
       </header>
     </div>
